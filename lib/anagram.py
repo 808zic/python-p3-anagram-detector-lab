d374219740ec65658ebc27586ad0e44b02808881
Georgemugo <georgepmash@gmail.com>
2024-06-05 08:54:34 +0300

    Adds validation

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, character):
+        self.character = character
+    
+    def match(self, character):
+        sorted_character = sorted(self.character)
+        return [w for w in character if sorted(w) == sorted_character]
